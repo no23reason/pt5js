f398b4f79f523ebb90db963094c1e03d2cd47191
Dan Homola <dan.homola@hotmail.cz>
2026-02-28 10:57:22 +0100

    feat: hide ncp by default
    The ncp is usually not interesting and only useful for debugging.
    Hide it by default and allow user to display it if needed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import type { FC } from "react";
+import { type FC, useState } from "react";
 import { FilePicker } from "./app/components/FilePicker.tsx";
 import { SavePt5Button } from "./app/components/SavePt5Button.tsx";
 import { NcpPreview } from "./app/components/NcpPreview.tsx";
 import { Pt5Preview } from "./app/components/Pt5Preview.tsx";
 import { useAppState } from "./app/hooks/useAppState.ts";
-import { IntlProvider } from "react-intl";
+import { FormattedMessage, IntlProvider } from "react-intl";
 import { DrawingPreview } from "./app/components/DrawingPreview.tsx";
 
 interface AppProps {
@@ -16,14 +16,20 @@ interface AppProps {
 export const App: FC<AppProps> = ({ messages, locale }) => {
     const ncpLines = useAppState((state) => state.ncpLines);
     const pt5Text = useAppState((state) => state.pt5Text);
+    const [showNcp, setShowNcp] = useState(false);
     return (
         <IntlProvider messages={messages} locale={locale}>
             <div className="controls-container">
                 <FilePicker />
                 <SavePt5Button />
+                {ncpLines?.length > 0 && (
+                    <button onClick={() => setShowNcp((v) => !v)}>
+                        <FormattedMessage id={showNcp ? "ncpPreview.hide" : "ncpPreview.show"} />
+                    </button>
+                )}
             </div>
             <div className="previews-container">
-                {ncpLines?.length > 0 && <NcpPreview />}
+                {showNcp && <NcpPreview />}
                 <Pt5Preview />
                 {pt5Text && <DrawingPreview />}
             </div>
